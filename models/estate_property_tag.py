6da8d7921cd356f3802a6f1902343f84523d2950
Hamid Mirzapour <h.r.mirzapour@gmail.com>
2024-10-06 00:37:41 +0330

    Create estate property tag model and access rights and action and menu, and many-to-many relation between this model and estate property model

---
+++ models/estate_property_tag.py
@@ -0,0 +1,16 @@
+from odoo import api, fields, models
+
+class EstatePropertyTag(models.Model):
+    _name = "estate.property.tag"
+    _description = "Estate Property Tag"
+
+    name = fields.Char(required=True)
+    color = fields.Integer("Color Index")
+
+    _sql_constraints = [
+        (
+            'unique_name',
+            'UNIQUE(name)',
+            'The name field should be unique.',
+        )
+    ]
