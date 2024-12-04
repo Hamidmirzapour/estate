933408238726c6f0ec43d484e02d06f093b08988
Hamid Mirzapour <h.r.mirzapour@gmail.com>
2024-12-04 21:50:49 +0330

    Add action_accept_offer and action_decline_offer and improve the user interface of estate_property and estate_property_offers tree and form view

--- models/estate_property_offer.py
+++ models/estate_property_offer.py
@@ -10,7 +10,7 @@ class EstatePropertyOffer(models.Model):
 
     partner_id = fields.Many2one("res.partner", string="Customer", required=True)
     price = fields.Float(required=True)
-    validity = fields.Integer()
+    validity = fields.Integer(default=7)
     deadline = fields.Date(compute='_compute_deadline', inverse='_inverse_deadline')
     state = fields.Selection(selection = [("accepted", "Accepted"), ("refused", "Refused"), ("waiting", "Waiting")],
                              default="waiting", required=True)
@@ -50,3 +50,24 @@ class EstatePropertyOffer(models.Model):
             if rec.deadline and rec.create_date:
                 if rec.deadline < rec.create_date.date():
                     raise ValidationError(_('Deadline can not be before create date'))
+
+    def action_accept_offer(self):
+        if self.estate_property_id:
+            self._validate_accept_offer()
+            self.estate_property_id.selling_price = self.price
+            self.state = 'accepted'
+            self.estate_property_id.state = 'offer_accepted'
+
+    def action_decline_offer(self):
+        self.state = 'refused'
+        if all(offer.state == 'refused' for offer in self.estate_property_id.estate_property_offer_ids):
+            self.estate_property_id.state = 'offer_received'
+            self.estate_property_id.selling_price = 0
+
+    def _validate_accept_offer(self):
+        offers = self.env['estate.property.offer'].sudo().search([
+            ('estate_property_id', '=', self.estate_property_id.id),
+            ('state', '=', 'accepted')
+        ])
+        if offers:
+            raise ValidationError(_('You have an accepted offer already'))
